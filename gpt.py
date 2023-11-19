3a5a020d73c8f43f842ebb54999eab027c08fdbd
Mert Tunc <mert.tunc@udemy.com>
2023-11-19 22:24:54 +0300

    Update gpt to v1

--- gpt.py
+++ gpt.py
@@ -1,4 +1,6 @@
-import openai
+from openai import OpenAI
+
+client = OpenAI()
 
 prompt = """
 You are given a database to query and a user prompt. Write a SQL query that answers the user's question.
@@ -20,16 +22,14 @@ class OpenAIWrapper:
                             database_tables=database_tables,
                             user_prompt=user_prompt))
 
-        response = openai.ChatCompletion.create(
-            model="gpt-3.5-turbo",
-            messages=[
-                {
-                    "role": "system",
-                    "content": "You are an SQL developer translating user prompts to SQL queries.",
-                },
-                {"role": "user", "content": prompt.format(database_name=database_name,
-                                                          database_tables=database_tables,
-                                                          user_prompt=user_prompt)},
-            ],
-        )
+        response = client.chat.completions.create(model="gpt-3.5-turbo",
+        messages=[
+            {
+                "role": "system",
+                "content": "You are an SQL developer translating user prompts to SQL queries.",
+            },
+            {"role": "user", "content": prompt.format(database_name=database_name,
+                                                      database_tables=database_tables,
+                                                      user_prompt=user_prompt)},
+        ])
         return response["choices"][0]["message"]["content"]
